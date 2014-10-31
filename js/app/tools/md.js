09c50f51b0a74439bc2dbcb050ab0fbcf7c6901f
forest <forest@tom.com>
2014-10-31 15:46:05 +0800

    mm

--- js/app/tools/md.js
+++ js/app/tools/md.js
@@ -1,4 +1,10 @@
+// document.write('<meta charset="utf8" />');
+// var meta=document.createElement('meta');
+// meta.setAttribute('charset', 'utf8');
+// document.querySelector('HEAD').appendChild(meta);
+
 (function(jsArray) {
+
   for (var i = 0; i < jsArray.length; i++) {
     document.write('<script src="' + jsArray[i] + '"></script>');
   }
@@ -6,7 +12,7 @@
 })([
   'http://libs.baidu.com/jquery/1.9.0/jquery.js',
   'http://libs.baidu.com/bootstrap/3.0.3/js/bootstrap.js',
-  'https://cdn.rawgit.com/foresthz/cdn/master/js/libs/markdown/markdown.js'
+  'https://rawgit.com/foresthz/cdn/master/js/libs/markdown/markdown.js'
   // 'http://cdn.bootcss.com/markdown.js/0.6.0-beta1/markdown.min.js'
 ]);
 
@@ -15,11 +21,10 @@
     document.write('<link rel="stylesheet" type="text/css" href="'+ cssArray[i] +'">')
   }
 })([
-  'https://cdn.rawgit.com/foresthz/cdn/master/css/app/tools/md.css' ,
   'http://libs.baidu.com/bootstrap/3.0.3/css/bootstrap.css'
 ]);
 
-var nodestr = '<div id="md_panel"> \
+var nodestr = '<hr><div id="md_panel"> \
     <div> \
         <a class="label label-success" id="show_md_btn">显示/隐藏 MD文章</a> \
     </div> \
@@ -41,4 +46,9 @@ function initMD() {
       $('#md_content').toggle();
     });
   });
+}
+
+// 无法在js中直接调用JQuery，因为此时js还未加载完毕，通过该方法，引用MarkDown功能又简化了不少。
+window.onload=function() {
+  initMD();
 }
