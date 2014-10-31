b1d15b540f9639c0e92c4402de36d1a3f0fa50cc
forest <forest@tom.com>
2014-10-31 16:01:36 +0800

    mm

--- js/app/tools/md.js
+++ js/app/tools/md.js
@@ -21,7 +21,8 @@
     document.write('<link rel="stylesheet" type="text/css" href="'+ cssArray[i] +'">')
   }
 })([
-  'http://libs.baidu.com/bootstrap/3.0.3/css/bootstrap.css'
+  'http://libs.baidu.com/bootstrap/3.0.3/css/bootstrap.css',
+  'https://rawgit.com/foresthz/cdn/master/css/app/tools/md.css'
 ]);
 
 var nodestr = '<hr><div id="md_panel"> \
